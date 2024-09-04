32e71198c9e7868eb22510aebec2b71aa38425ee
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-09-04 11:49:44 -0400

    adding some style and adjusting the content so it looks like a website

--- models/contactData.js
+++ models/contactData.js
@@ -2,10 +2,10 @@
 
 const contactData = {
     title: "CONTACT",
-    owner: "Laura Peckyno",
-    phone: "206-202-5512",
-    address: "123 First Ave",
-    email: "laura.peckyno@something.com"
+    owner: "Reach out if you'd like to discuss your project and how we can assist you.",
+    phone: "Phone:  206-202-5512",
+    address: "Address:  123 First Ave, Pittsburgh PA",
+    email: "Email:  laura.peckyno@something.com"
 }
 
 module.exports = contactData
--- models/homeData.js
+++ models/homeData.js
@@ -2,9 +2,9 @@
 
 const homeData = {
     title: "HOME",
-    intro: "Insert intro here.",
-    ourskills: "Check out our services page to see what we can do for you!",
-    other: "Add some other text."
+    intro: "Here at Edit This, we've been creating and editing content for more than 20 years.",
+    ourskills: "We're experts in both digital and print content creation, preparation, and distribution. If you're developing a project and need assistance with any part of the process, we can help. Check out our services page to see what we can do for you!",
+    other: "Reach out on our contact page. We look forward to hearing from you. "
 }
 
 module.exports = homeData
